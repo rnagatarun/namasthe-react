d3086cfc817b90d20eb0675258058c5373daf316
NagaTarun <rnagatarun@gmail.com>
2025-04-12 16:57:59 +0530

    add Body component for the app

---
+++ Chapter-05 Let's get Hooked/src/components/Body.js
@@ -0,0 +1,20 @@
+import RestaurantCard from "./RestaurentCard";
+import restaurantList from "../utils/mockData";
+
+const Body = () => {
+
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {restaurantList.map((restaurant) => {
+          return (
+            <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
