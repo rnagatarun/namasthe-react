78d04f05992b35820a2c7eeb028976d2d749c45a
NagaTarun <rnagatarun@gmail.com>
2025-04-13 14:58:52 +0530

    use useState to handle dynamic UI update

--- Chapter-05 Let's get Hooked/src/components/Body.js
+++ Chapter-05 Let's get Hooked/src/components/Body.js
@@ -1,13 +1,27 @@
 import RestaurantCard from "./RestaurentCard";
 import restaurantList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList);
 
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredListOfRestaurants = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurants(filteredListOfRestaurants);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {restaurantList.map((restaurant) => {
+        {listOfRestaurants.map((restaurant) => {
           return (
             <RestaurantCard key={restaurant.data.id} resData={restaurant} />
           );
