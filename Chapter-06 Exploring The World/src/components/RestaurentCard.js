d4c601956143abe8a5d1e6b041c3de999372e6ac
NagaTarun <rnagatarun@gmail.com>
2025-04-15 01:12:47 +0530

    update cdn url for image and initiate proper data retieveal

--- Chapter-06 Exploring The World/src/components/RestaurentCard.js
+++ Chapter-06 Exploring The World/src/components/RestaurentCard.js
@@ -3,14 +3,14 @@ import { CDN_URL } from "../utils/constants";
 const RestaurantCard = (props) => {
   const { resData } = props;
   const { cloudinaryImageId, name, cuisines, avgRating, costForTwo } =
-    resData.data;
+    resData.info;
   return (
     <div className="res-card" style={{ backgroundColor: "lightgray" }}>
       <img className="res-logo" src={CDN_URL+cloudinaryImageId} alt="Food Image" />
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
-      <h4>{avgRating}</h4>
-      <h4>{costForTwo / 100}</h4>
+      <h4>{avgRating} star</h4>
+      <h4>{costForTwo}</h4>
     </div>
   );
 };
