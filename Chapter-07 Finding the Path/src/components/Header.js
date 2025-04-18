1a8811d7cb4e7aff0640cd4974a244e68356f687
NagaTarun <rnagatarun@gmail.com>
2025-04-18 16:44:32 +0530

    add Login button without the functionality, updates name onClick

--- Chapter-07 Finding the Path/src/components/Header.js
+++ Chapter-07 Finding the Path/src/components/Header.js
@@ -1,6 +1,8 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+  const [loginStatus, setLoginStatus] = useState("Login");
   return (
     <div className="header">
       <div className="logo-container">
@@ -13,6 +15,17 @@ const Header = () => {
           <li>About</li>
           <li>Contact</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+                loginStatus === "Login"
+                  ? setLoginStatus("Logout")
+                  : setLoginStatus("Login")
+              }
+            }
+          >
+            {loginStatus}
+          </button>
         </ul>
       </div>
     </div>
