cbf3e31f1d8672119e31057edce3ac8a968ba0dc
NagaTarun <rnagatarun@gmail.com>
2025-04-23 18:09:19 +0530

    add Classbased component userClass

---
+++ Chapter-08 Lets get Classy/src/components/UserClass.js
@@ -0,0 +1,14 @@
+import "./User.css";
+import React from "react";
+
+class UserClass extends React.Component {
+  render() {
+    return (
+      <div className="user-card">
+        <h2>Name: Tarun</h2>
+        <h3>Location: Banglore</h3>
+        <h4>contact: @naga_tarun</h4>
+      </div>
+    );
+  }
+}
