9ce5b84d73d33489550a1d4d1d189379274493d9
NagaTarun <rnagatarun@gmail.com>
2025-04-23 23:59:35 +0530

    make class based components and explore lifecycle methods

--- Chapter-08 Lets get Classy/src/components/About.js
+++ Chapter-08 Lets get Classy/src/components/About.js
@@ -1,17 +1,30 @@
-import User from './User';
-import UserClass from './UserClass';
+import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
 
-const About = () => {
-    return(
-        <div className="about">
-            <h1>About Us</h1>
-            <p>This is the about page of our application.</p>
-            <p>We are dedicated to providing the best service possible.</p>
-            <User name={"Naga Tarun(Function)"} location ={"Banglore"}/>
-            
-            <UserClass name={"Naga Tarun(Class)"} location ={"Banglore"}/>
-        </div>
+class About extends Component {
+  constructor(props) {
+    console.log("Parent constructor --(About.js Class)")
+    super(props);
+  }
+
+  componentDidMount(){
+    console.log("Parent componentDidMount --(About.js Class)")
+  }
+
+  render() {
+    console.log("Parent render --(About.js Class)")
+    return (
+      <div className="about">
+        <h1>About Us</h1>
+        <p>This is the about page of our application.</p>
+        <p>We are dedicated to providing the best service possible.</p>
+        <User name={"Naga Tarun(Function)"} location={"Banglore"} />
+
+        <UserClass name={"Naga Tarun(Class)"} location={"Banglore"} />
+      </div>
     );
+  }
 }
 
 export default About;
--- Chapter-08 Lets get Classy/src/components/UserClass.js
+++ Chapter-08 Lets get Classy/src/components/UserClass.js
@@ -3,12 +3,31 @@ import React from "react";
 
 class UserClass extends React.Component {
   constructor(props) {
+    console.log("Child construtor--(UserClass)")
     super(props);
+    this.state = {
+      count: 0,
+    }
   }
+
+  
+  componentDidMount(){
+    console.log("Child componentDidMount --(UserClass)")
+    //Do API call here.
+  }
+
   render() {
-    const{name} = this.props
+    console.log("Child render --(UserClass)")
+    const{name} = this.props;
+    const{count} = this.state;
     return (
       <div className="user-card">
+        <h1>Count: {count}</h1>
+        <button onClick={() => {
+          //Never update STATE variable directly like "this.state.count = this.state.count + 1"
+          //In setState() pass the object{} and update logic inside it like {count: this.state.count + 1}
+          this.setState({count: this.state.count + 1})
+        }}>Increase Count</button>
         <h2>Name: {name} </h2>
         <h3>Location:{this.props.location} </h3>
         <h4>contact: @naga_tarun</h4>
