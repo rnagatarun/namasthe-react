3bec4ed9157f3712d5ee96adc8258926c9004b75
NagaTarun <rnagatarun@gmail.com>
2025-04-23 18:08:58 +0530

    add functional component User for About.js

--- Chapter-08 Lets get Classy/src/components/About.js
+++ Chapter-08 Lets get Classy/src/components/About.js
@@ -1,9 +1,12 @@
+import User from './User';
+
 const About = () => {
     return(
         <div className="about">
             <h1>About Us</h1>
             <p>This is the about page of our application.</p>
             <p>We are dedicated to providing the best service possible.</p>
+            <User />
         </div>
     );
 }
---
+++ Chapter-08 Lets get Classy/src/components/User.js
@@ -0,0 +1,13 @@
+import './User.css';
+
+const User = () => {
+    return(
+        <div className="user-card">
+            <h2>Name: Tarun</h2>
+            <h3>Location: Banglore</h3>
+            <h4>contact: @naga_tarun</h4>
+        </div>
+    );
+}
+
+export default User;
