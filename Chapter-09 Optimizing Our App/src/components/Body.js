3fdd32aa46b9b2b05a1f7687d7c5420a46df3d1a
NagaTarun <rnagatarun@gmail.com>
2025-04-24 19:28:44 +0530

    optimize app by seperation of concerns to fetch restauants by useRestaurants

--- Chapter-09 Optimizing Our App/src/components/Body.js
+++ Chapter-09 Optimizing Our App/src/components/Body.js
@@ -1,31 +1,18 @@
 import RestaurantCard from "./RestaurentCard";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Shimmer from "./Shimmer.js";
 import { Link } from "react-router-dom";
+import useRestaurants from "../utils/useRestaurants.js";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState([]);
   const [searchText, setSearchText] = useState("");
+  const listOfRestaurants = useRestaurants();
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9371531&lng=77.6901166&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-    );
-    const json = await data.json();
-    // console.log("Json data", json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-
-    setListOfRestaurants(
-      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
-    setFilteredRestaurants(
-      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
-    );
-  };
+  // Update filteredRestaurants immediately after listOfRestaurants is fetched
+  if (filteredRestaurants.length === 0 && listOfRestaurants.length > 0) {
+    setFilteredRestaurants(listOfRestaurants);
+  }
 
   // Conditional rendering
   return listOfRestaurants.length === 0 ? (
