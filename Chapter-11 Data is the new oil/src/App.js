34ed252ee4eae144b269313051ddb3172c21357c
NagaTarun <rnagatarun@gmail.com>
2025-04-27 00:06:05 +0530

    provide the context for whole App and set userName via a dummy API call using useEffect

--- Chapter-11 Data is the new oil/src/App.js
+++ Chapter-11 Data is the new oil/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
@@ -9,15 +9,39 @@ import Error from "./components/Error";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import "../index.css";
 import RestaurantMenu from "./components/RestaurantMenu";
+import UserContext from "./utils/UserContext";
 
+//Lazy loading import
 const Grocery = lazy(() => import("./components/Grocery"));
 
 const AppLayout = () => {
+  const [userName, setUserName] = useState();
+
+  //authentication
+  useEffect(() => {
+    //Make an API call and send userName and Passord
+    const data = {
+      name: "Naga Tarun",
+    };
+    setUserName(data.name);
+  }, []);
+
   return (
-    <div className="app">
-      <Header />
-      <Outlet />
-    </div>
+    //Outside this context "Default User" is used
+    <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+      {/* Entire App uses the value={{ loggedInUser: userName }} i.e NagaTarun */}
+      <div className="app">
+        {/*
+         But header is wrapped inside other context with user RNT, so RNT is used in header
+        <UserContext.Provider value={{ loggedInUser: "RNT" }}>
+          <Header />
+        </UserContext.Provider>
+         */}
+         
+        <Header />
+        <Outlet />
+      </div>
+    </UserContext.Provider>
   );
 };
 
