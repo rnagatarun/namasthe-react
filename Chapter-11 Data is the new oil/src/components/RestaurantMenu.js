1872db6b1243cf759eddc0b8b8f6df44164c9ded
NagaTarun <rnagatarun@gmail.com>
2025-04-26 14:13:10 +0530

    get categories of restaurantMenu from API and pass data as prop to RestaurantCategory

--- Chapter-11 Data is the new oil/src/components/RestaurantMenu.js
+++ Chapter-11 Data is the new oil/src/components/RestaurantMenu.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import "./RestaurantMenu.css";
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 
 const RestaurantMenu = () => {
   const { resId } = useParams();
@@ -10,46 +10,33 @@ const RestaurantMenu = () => {
 
   // console.log("Restaurant ID:", resId);
 
-  const { name, cuisines, costForTwoMessage } =
+  const { name, cuisines, costForTwo } =
     resInfo?.cards[2]?.card?.card?.info ?? {};
 
-  const itemCards =
-    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card
-      ?.card ?? {};
-  console.log(itemCards);
+  const categories =
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards.filter(
+      (category) =>
+        category.card?.card?.["@type"] ===
+        "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+    );
+  console.log(categories);
+
+  // const itemCards =
+  //   resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card ?? {};
+  // console.log(itemCards);
 
   return resInfo === null ? (
     <Shimmer />
   ) : (
-    <div className="menu">
-      <h2>Menu</h2>
-      <h3>{name}</h3>
-      <ul>
-        {itemCards?.itemCards?.length > 0 ? (
-          itemCards.itemCards.map((item) => (
-            <li key={item.card.info.id}>
-              <div className="menu-item">
-                <div className="menu-item-content">
-                  <h3>{item.card.info.name}</h3>
-                  <span>₹{item.card.info.price / 100}</span>
-                  <p>{item.card.info.description}</p>
-                </div>
-                <img
-                  src={
-                    item.card.info.imageId
-                      ? `https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/${item.card.info.imageId}`
-                      : "https://via.placeholder.com/300x300"
-                  }
-                  alt={item.card.info.name || "Menu item"}
-                  className="menu-item-image"
-                />
-              </div>
-            </li>
-          ))
-        ) : (
-          <p>No items available</p>
-        )}
-      </ul>
+    <div className="menu text-center">
+      <h1 className="font-bold my-6 text-2xl">{name}</h1>
+      <p className="font-bold text-lg">
+        {cuisines?.join(", ")} - {costForTwo / 100}
+      </p>
+
+      {categories?.length > 0 && categories.map((category,index) => (
+        <RestaurantCategory key={category.card?.card?.title || index} data={category.card?.card}/>
+      ))}
     </div>
   );
 };
