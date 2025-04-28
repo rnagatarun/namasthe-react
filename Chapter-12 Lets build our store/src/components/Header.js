6b1b49454523c7168dc9cd87031f2aa9ff42ba8b
NagaTarun <rnagatarun@gmail.com>
2025-04-28 23:16:54 +0530

    subscribe store using useSelectorand display itemcount on header and build cart component to display items from cartSlice and add functionality to clear cart

--- Chapter-12 Lets build our store/src/components/Header.js
+++ Chapter-12 Lets build our store/src/components/Header.js
@@ -3,11 +3,16 @@ import { Link } from "react-router-dom";
 import { LOGO_URL } from "../utils/constants";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import UserContext from "../utils/UserContext";
+import { useSelector } from 'react-redux';
 
 const Header = () => {
   const [loginStatus, setLoginStatus] = useState("Login");
   const onlineStatus = useOnlineStatus();
   const { loggedInUser } = useContext(UserContext);
+
+  //Subscribing to the store using a Selector
+  const cartItems = useSelector((store) => store.cart.items);
+  console.log(cartItems);
 
   return (
     <div className="flex justify-between bg-pink-100 shadow-lg">
@@ -30,8 +35,8 @@ const Header = () => {
           <li className="px-4">
             <Link to="/grocery">Grocery</Link>
           </li>
-          <li className="px-4">
-            <Link to="/cart">Cart</Link>
+          <li className="px-4 font-bold">
+            <Link to="/cart">Cart-({cartItems.length} items)</Link>
           </li>
           <button
             className="login"
